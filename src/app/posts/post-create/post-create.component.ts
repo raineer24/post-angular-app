92b5bf14e0935df7d730fcbbcc32f0bb406954b3
Raineer <delaritaraineer81@gmail.com>
2019-03-21 03:26:16 +0800

    ngmodel name

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import  { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-post-create',
@@ -14,10 +15,11 @@ export class PostCreateComponent implements OnInit {
     
     constructor() { }
 
-    onAddPost(){
+    onAddPost(form: NgForm){
+        
         const post: Post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
+            title: form.value.title,
+            content: form.value.content
         };
         this.postCreated.emit(post);
     }

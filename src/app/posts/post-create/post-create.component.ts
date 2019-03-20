709393bfb38fab5fb784919728f0946e6f3256a1
Raineer <delaritaraineer81@gmail.com>
2019-03-21 03:37:25 +0800

    mat error msg

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -17,7 +17,7 @@ export class PostCreateComponent implements OnInit {
 
     onAddPost(form: NgForm){
         if (form.invalid) {
-        
+            return;
         }
         const post: Post = {
             title: form.value.title,

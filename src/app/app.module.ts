31a0b9f5d679718d2c5ea7e83b0ecb8e604d4aa1
Raineer <delaritaraineer81@gmail.com>
2019-03-21 01:49:24 +0800

    add postlistcomponenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,14 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: 'name.component.html'
+})
+
+export class PostListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}

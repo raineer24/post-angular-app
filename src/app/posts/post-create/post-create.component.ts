7b2b10d488127c4a449cbae32c557c4da3c45ce3
Raineer <delaritaraineer81@gmail.com>
2019-03-20 17:44:16 +0800

    onAddPost()

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,20 @@
+import  { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html'
+})
+
+export class PostCreateComponent implements OnInit {
+    newPost = 'NO CONTENT';
+
+    constructor() { }
+
+    onAddPost(postInput: HTMLTextAreaElement){
+        console.log(postInput);
+        console.dir(postInput);
+        this.newPost = 'The user\'s post';
+    }
+
+    ngOnInit() { }
+} 

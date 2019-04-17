5afa3528503da013618b7196d1920b55c6c7d66c
raineer <delaritaraineer81@gmail.com>
2019-04-17 14:54:50 +0800

    remove comments

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -44,20 +44,4 @@ export class PostsService {
       })
     );
   }
-
-  // private posts: Post[] = [];
-  // private postsUpdated = new Subject<Content[]>();
-
-  // getPosts() {
-  //     return [...this.posts];
-  // }
-  // getPostUpdateListener() {
-  //     return this.postsUpdated.asObservable();
-  // }
-
-  // addPost(title: string, content: string) {
-  //     const post: Post = { title: title, content: content };
-  //     this.posts.push(post);
-  //     this.postsUpdated.next([...this.posts]);
-  // }
 }

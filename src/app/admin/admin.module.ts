81dd94be69386e967196f464a7563539bec318e8
raineer <delaritaraineer81@gmail.com>
2019-04-18 22:46:25 +0800

    coreModule post services

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { ProductsComponent, AdminComponent } from "./pages";
 import { AdminRoutingModule } from "./admin.routing.module";
+import { MatTableModule } from "@angular/material";
 
 @NgModule({
   declarations: [ProductsComponent, AdminComponent],
-  imports: [AdminRoutingModule],
+  imports: [AdminRoutingModule, MatTableModule],
   providers: []
 })
 export class AdminModule {}

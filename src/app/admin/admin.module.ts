f8e0ee32808a29bcaf1164ddf50f31fcb9327fb9
raineer <delaritaraineer81@gmail.com>
2019-04-18 23:24:29 +0800

    add-product component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { ProductsComponent, AdminComponent } from "./pages";
 import { AdminRoutingModule } from "./admin.routing.module";
-import { MatTableModule } from "@angular/material";
+import { MatTableModule, MatButtonModule } from "@angular/material";
+import { ProductAddComponent } from "./pages/product/product-add/product-add.component";
 
 @NgModule({
-  declarations: [ProductsComponent, AdminComponent],
-  imports: [AdminRoutingModule, MatTableModule],
+  declarations: [ProductsComponent, AdminComponent, ProductAddComponent],
+  imports: [AdminRoutingModule, MatTableModule, MatButtonModule],
   providers: []
 })
 export class AdminModule {}
--- src/app/admin/admin.routing.module.ts
+++ src/app/admin/admin.routing.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { ProductsComponent, AdminComponent } from "./pages";
+import { ProductAddComponent } from "./pages/product/product-add/product-add.component";
 
 const routes: Routes = [
   {
     path: "admin",
     component: AdminComponent,
     pathMatch: "prefix",
-    children: [{ path: "products", component: ProductsComponent }]
-  },
-  {
-    path: "",
-    redirectTo: "admin/products",
-    pathMatch: "full"
+    children: [
+      { path: "products", component: ProductsComponent },
+      { path: "add-product", component: ProductAddComponent }
+    ]
   }
+  // {
+  //   path: "",
+  //   redirectTo: "admin/products",
+  //   pathMatch: "full"
+  // },
+  // {
+  //   path: "",
+  //   redirectTo: "admin/add-product",
+  //   pathMatch: "full"
+  // }
 ];
 
 // /app/src/admin/pages/admin.component.ts

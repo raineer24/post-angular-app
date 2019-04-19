ad74f2b55415a9ff68871fc68830737a096e8fd7
raineer <delaritaraineer81@gmail.com>
2019-04-19 16:19:04 +0800

    angular cli

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const http = require("http");
+const path = require("path");
+
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/*", function(req, res) {
-  res.sendFile(__dirname + "/dist/index.html");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
-app.listen(process.env.PORT || 4200);
-console.log("SERVER RUNNING...");
+const port = process.env.PORT || 3000;
+app.set("port", port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log("running"));

baae49945e916738a13eaae7df31b3e4bdfa0c97
raineer <delaritaraineer81@gmail.com>
2019-04-19 16:05:50 +0800

    working heroku

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/dist"));
-app.listen(process.env.PORT || 8080);
+
+app.get("/*", function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(process.env.PORT || 4200);
+console.log("SERVER RUNNING...");
